e29b4aa58a212c34ea47373857d3cbca4ed8b4ff
tnt <45692264+tuna2134@users.noreply.github.com>
2022-01-02 09:34:16 +0900

    Create main.py

---
+++ tinyurl_ga/main.py
@@ -0,0 +1,13 @@
+import requests
+
+def create(url):
+    r = requests.post("https://tinyurl.ga/api", json = {"url": url})
+    return result(r.json())
+    
+class result:
+    def __init__(self, data):
+        self.data = data
+        
+    @property
+    def url(self) -> str:
+        return "https://tinyurl.ga/" + data["url"]
